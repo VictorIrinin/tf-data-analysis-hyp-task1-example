b787615162a370006582d7832a339bbf52a0747f
VictorIrinin <129628121+VictorIrinin@users.noreply.github.com>
2023-04-11 17:02:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import norm
 
 chat_id = 5780444792 # Ваш chat ID, не меняйте название переменной
 
@@ -11,16 +11,10 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    res = False
-    x_k = x_success / x_cnt
-    y_k = y_success / y_cnt    
-    if x_k > y_k:
-        res = True
-    elif x_k < y_cnt:
-        res = False
-    else:
-        if x_cnt <= y_cnt:
-            res = True
-        else:
-            res = False
-    return res # Ваш ответ, True или False
+    level=0.03
+    p1=x_success/x_cnt
+    p2=y_success/y_cnt
+    p=(x_success+y_success)/(x_cnt+y_cnt)
+    z=(p2-p1)/((p*(1-p)*((1/x_cnt)+(1/y_cnt)))**0/5)
+    p_value=norm.cdf(z)
+    return True if p_value<level else False
